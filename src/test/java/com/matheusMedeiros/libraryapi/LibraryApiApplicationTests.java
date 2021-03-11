e34d21c63ac16200a3081eb7e403f90d8fbd4f01
Matheus Medeiros <matheus.cardosoibm.com@Matheuss-MacBook-Pro.local>
2021-03-11 10:42:30 -0300

    first commit

---
+++ src/test/java/com/matheusMedeiros/libraryapi/LibraryApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.matheusMedeiros.libraryapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LibraryApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
